cd938795d2f085d9a1ad0f58182dadc0c7a7f4b4
Wolfy-22 <bubblarr@gmail.com>
2026-03-25 12:33:55 -0400

    First Commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	const connectString = "amqp://guest:guest@localhost:5672/"
+
+	connection, err := amqp.Dial(connectString)
+	if err != nil {
+		fmt.Printf("error creaing connection to RabbitMQ: %v", err)
+	}
+	defer connection.Close()
+
+	fmt.Println("Connection successful!!")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Program shutting down")
+	os.Exit(1)
 }
